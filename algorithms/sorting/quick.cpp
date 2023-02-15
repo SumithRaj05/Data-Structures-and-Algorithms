b2d89d62bb24cfb659d4b1a04681d1ad7e26c3e7
SumithRaj <sumithraj05@SumithRaj.sr>
2023-02-15 19:49:14 +0530

    updated

--- algorithms/sorting/quick.cpp
+++ algorithms/sorting/quick.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int partition(int arr[], int start, int end){
     int pivot = arr[end];
-    int pin = start;
-    for(int i = start; i<end-1; i++){
-        if(arr[i]<=pivot){
-            swap(arr[pin], arr[i]);
-            pin++;
+    int j = start-1;
+    for(int i = start; i<end; i++){
+        if(arr[i] <= pivot){
+            j++;
+            swap(arr[j], arr[i]);
         }
     }
-    swap(arr[pin], arr[end]);
-    return pin;
+    swap(arr[j+1], arr[end]);
+    return j+1;
 }
 
 void quickSort(int arr[], int start, int end){
-    if(start>=end)
+    if(start >= end)
         return;
     int pin = partition(arr, start, end);
     quickSort(arr, start, pin-1);
@@ -23,7 +23,7 @@ void quickSort(int arr[], int start, int end){
 }
 
 int main(){
-    int arr[] = {2,5,1,4,3};
+    int arr[] = {5,4,3,2,1};
     int n = sizeof(arr)/sizeof(arr[0]);
     quickSort(arr, 0, n-1);
     for(int i: arr)
