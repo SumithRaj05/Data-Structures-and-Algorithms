2c4cc4820ea8286e6d875c1924f3543184f0fa68
SumithRaj <sumithraj05@SumithRaj.sr>
2023-02-25 13:05:53 +0530

    updated

--- revise.cpp
+++ revise.cpp
@@ -1,55 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element and indicates
-                 
-    for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            swap(arr[i], arr[j]);
+int partition(vector<int> &a, int low, int high){
+    int pivot = a[high];
+    int i = high-1;
+    for(int j=low; j<high; j++){
+        if(a[j]>=pivot){
+            swap(a[j],a[i]);
+            i--;
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
+    swap(a[i+1],a[high]);
+    return i+1;
 }
 
-void quickSort(int arr[], int start, int end){
-    if(start>=end)
+void quicksort(vector<int> &a, int low, int high){
+    if(low>=high)
         return;
-    int pin = partition(arr, start, end);
-    quickSort(arr, start, pin-1);
-    quickSort(arr, pin+1, end);
+    int pivot = partition(a,low,high);
+    quicksort(a,low,pivot-1);
+    quicksort(a,pivot+1,high);
 }
 
 int main(){
-    int n,m,u,v;
-    cin>>n>>m;
-    vector<int> graph[];
-    for(int i=0; i<m; i++){
-        cin>>u>>v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+    vector<int> a = {5,4,3,2,1};
+    int n = a.size();
+    quicksort(a,0,n-1);
+    for(int i:a){
+        cout<<i<<"\t";
     }
-    vector<int> visited(n,0);
-    stack<int> s;
-    int i=0;
-    s.push(i);
-    visited[i]=1;
-    while(s.size()<=n){
-        for(int j=0; j<n; j++){
-            if(visited[j]!=1){
-                s.push(j);
-                visited[j]=1;
-            }
-        }
-    }
-    while(!s.empty()){
-        cout<<s.top()<<endl;
-        s.pop();
-    }
+    cout<<endl;
     return 0;
 }
